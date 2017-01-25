f1e125d40286cb52c1da1703a87ff27218cb1242
max nesler <maxnesler@gmail.com>
2017-01-25 00:40:29 -0500

    added this context menu, its not working yet

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,81 +1,78 @@
 import React from 'react';
-//import cardStyle from './card.css'
+import {ContextMenu, MenuItem, ContextMenuTrigger} from "react-contextmenu";
+import cardStyle from './card.css'
+import reactContextmenuStyle from './react-contextmenu.css'
+
 export default class Card extends React.Component {
-     constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
-          id: -1,
-          backImg: '../img/back.jpg',
-          frontImg: '..img/placeholder.jpg',
-          faced: false,
-          flop: false
-        };
+      id: -1,
+      backImg: '../img/back.jpg',
+      frontImg: '..img/placeholder.jpg',
+      faced: false,
+      tapped: false,
+      enlarged: false,
+      imageShown: '../img/back.jpg'
+    };
 
-        this.flip = this.flip.bind(this);
-        this.flop = this.flop.bind(this);
-        this.enlarge = this.enlarge.bind(this);
-
-        //do i need to do onClick?
-
+    this.flip = this.flip.bind(this);
+   // this.handleClick = this.handleClick.bind(this);
+    this.tapped = this.tapped.bind(this);
+    this.enlarge = this.enlarge.bind(this);
   }
   componentWillMount() {
-     this.setState({
-        id: this.props.card.id,
-        frontImg: this.props.card.front,
-        faced: this.props.card.faced,
-      });
+    this.setState({id: this.props.card.id, frontImg: this.props.card.front, faced: this.props.card.faced});
   }
-  handleClick = (e) => {
-      console.log(e.nativeEvent.which); // type: left, value - 1
-  console.log(e.nativeEvent.which); // type: right, value - 3
-  console.log(e.type); // type: left, value - click 
-  console.log(e.type); // type: right, value - contextm
-    this.flip()
+  handleClick = (e, data, element) => {
+    console.log(data)
   }
   handleLeftClick = () => {}
   handleRightClick = () => {}
   handleMiddleClick = () => {}
 
-  flop = () => { this.state.flop ? this.setState({flop: false}) : this.setState({flop: true}) }
-  flip = () => { this.state.faced ? this.setState({faced: false}) : this.setState({faced: true}) }
-  enlarge = () => { this.state.enlarged ? this.setState({enlarged: false}) : this.setState({enlarged: true}) }
-  toggleHover = () => { this.setState({hover: !this.state.hover}) }
+  tapped = () => { this.setState({tapped: !this.state.tapped})
+      console.log('tapped')
+  }
+  flip = () => {
+    this.state.faced
+      ? this.setState({faced: false})
+      : this.setState({faced: true})
+  }
+  enlarge = () => {
+    this.state.enlarged
+      ? this.setState({enlarged: false})
+      : this.setState({enlarged: true})
+  }
+  //toggleHover = () => { this.setState({hover: !this.state.hover}) }
 
   render() {
-    var cardStyle = {
-    margin: '2px',
-    width: '130px',
-    transition: '.3s all',  /* rotate gradually instead of instantly */
-    float: 'left',
-    
-
-
-//.card:hover {
-//    -webkit-transform: rotate(90deg);  /* to support Safari and Android browser */
-//    -ms-transform: rotate(90deg);      /* to support IE 9 */
-//    transform: rotate(90deg);
-//}
-
-
-    }
-    if(this.state.faced) {
-    return (
-      <span>
-        <div class={'card'} style={cardStyle} onClick={this.handleClick}>
-          <img width={'130px'} src={this.state.frontImg} />
-        </div>
-      </span>
-
-    );
-    } else {
       return (
-        <span>
-          <div class={'card'} style={cardStyle} onClick={this.handleClick}>
-            <img width={'130px'} src={this.state.backImg} />
-          </div>
-      </span>
-    );
-    }
+        <div>
+          <ContextMenuTrigger id='cardMenu'>
+            <span>
+              <div className={'card'} style={cardStyle}>
+                <img width={'130px'} src={this.state.frontImg}/>
+              </div>
+            </span>
+          </ContextMenuTrigger>
 
-  }
+          <ContextMenu id="cardMenu">
+            <MenuItem onClick={this.tapped} data={{item: 'item 1'}}>
+              Tap
+            </MenuItem>
+            <MenuItem onClick={this.flip} data={{item: 'item 2'}}>
+              Flip Over
+            </MenuItem>
+            <MenuItem divider/>
+            <MenuItem onClick={this.handleClick} data={{item: 'item 3'}} disabled={true}>
+              Discard
+            </MenuItem>
+            <MenuItem onClick={this.handleClick} data={{item: 'item 4'}}>
+              Graveyard
+            </MenuItem>
+          </ContextMenu>
+        </div>
+      )
+    }
 }
